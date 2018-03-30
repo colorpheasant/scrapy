f5a4ea8419964d64cb02ffb235716074a4c8fcf5
xwp <xwp_fullstack@163.com>
2018-03-30 20:42:30 +0800

    配置mysql数据哭

--- manage.py
+++ manage.py
@@ -1,14 +1,18 @@
 # -*-coding=utf-8 -*-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app =Flask(__name__)
 
 class Config(object):
     """工程配置信息"""
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI ='mysql://root:mysql@127.0.0.1:3306/ihome_07'
+    SQLALCHEMY_TRACK_MODIFICATIONS =False
 
+app =Flask(__name__)
 app.config.from_object(Config)
-
+#創建連接到mysql数据哭的对象
+db = SQLAlchemy(app)
 @app.route('/index')
 def index():
     return 'index'
