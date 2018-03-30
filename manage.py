6a4e9a8c485cef6bcdbc9b8240afa19177d068e1
xwp <xwp_fullstack@163.com>
2018-03-30 21:34:39 +0800

    开启csrf安全

--- manage.py
+++ manage.py
@@ -1,20 +1,31 @@
 # -*-coding=utf-8 -*-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
-
+import redis
+from flask_wtf.csrf import CsrfProtect
+import os,base64
 class Config(object):
     """工程配置信息"""
     DEBUG = True
+    # SECRET_KEY = '12345UHSDIFHIO@123'
     SQLALCHEMY_DATABASE_URI ='mysql://root:mysql@127.0.0.1:3306/ihome_07'
     SQLALCHEMY_TRACK_MODIFICATIONS =False
+    #配置redis数据:
+    REDIS_HOST='127.0.0.1'
+    REDIS_PORT=6379
 
 app =Flask(__name__)
 app.config.from_object(Config)
 #創建連接到mysql数据哭的对象
 db = SQLAlchemy(app)
-@app.route('/index')
+
+redis_store =redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
+
+CsrfProtect(app)
+@app.route('/index',methods=['GET','POST'])
 def index():
+    #测试redis数据哭
+    redis_store.set('name','sz007')
     return 'index'
 
 if __name__ == '__main__':
