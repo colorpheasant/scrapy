43837dba504958cbad5146db7d60709fbf44fcd5
xwp <xwp_fullstack@163.com>
2018-03-30 16:23:46 +0800

    项目初始化,立项

---
+++ manage.py
@@ -0,0 +1,16 @@
+# -*-coding=utf-8 -*-
+from flask import Flask
+
+app =Flask(__name__)
+
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
+
+app.config.from_object(Config)
+@app.route('/index')
+def index():
+    return 'index'
+
+if __name__ == '__main__':
+    app.run(debug=True)
